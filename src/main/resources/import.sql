505927185a72ecb229524b78d55951074b82e810
Luiz Gustavo Costa <luiz.gustavo.costa82@gmail.com>
2022-01-04 22:30:54 -0300

    Initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO known_fruits(id, name) VALUES (1, 'Cherry');
+INSERT INTO known_fruits(id, name) VALUES (2, 'Apple');
+INSERT INTO known_fruits(id, name) VALUES (3, 'Banana');
+INSERT INTO known_fruits(id, name) VALUES (4, 'Papaya');
+INSERT INTO known_fruits(id, name) VALUES (5, 'Jabuticaba');
